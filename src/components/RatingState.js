0eaa640eaf5c54aeb56b7a91585c34b0e67a7f27
Andrea Castillo <castiyeauxdev@gmail.com>
2022-03-26 22:32:16 -0500

    Moved images folder to /src due to 'out of scope' - stuck on making buttons work

--- src/components/RatingState.js
+++ src/components/RatingState.js
@@ -1,15 +1,25 @@
 import './InteractiveRating.css';
+import { ReactComponent as StarIcon } from '../images/icon-star.svg';
 
 function RatingState() {
     return (
         <div className="rating-state">
-            <h2>How did we do?</h2>
+            <StarIcon className="star-icon"></StarIcon>
+            <div className="rating-question">
+                <h2>How did we do?</h2>
 
-            <p className="gray-text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+                <p className="gray-text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+            </div>
 
-            <p className="gray-text">1 2 3 4 5</p>
+            <div className="rating-buttons">
+                <p className="gray-text rating-button">1</p>
+                <p className="gray-text rating-button">2</p>
+                <p className="gray-text rating-button">3</p>
+                <p className="gray-text rating-button">4</p>
+                <p className="gray-text rating-button">5</p>
+            </div>
 
-            <button>Submit</button>
+            <button className="submit-button">Submit</button>
         </div>
     );
 }
--- src/components/ThankYouState.js
+++ src/components/ThankYouState.js
@@ -1,13 +1,15 @@
 import './InteractiveRating.css';
+import { ReactComponent as ThankYouIcon } from '../images/illustration-thank-you.svg';
 
 function ThankYouState() {
     return (
         <div className="thank-you-state">
-            <p>You selected <strong>Add rating here</strong> out of 5</p>
+            <ThankYouIcon></ThankYouIcon>
+            <p className="gray-text">You selected <strong>Add rating here</strong> out of 5</p>
 
-            <p>Thank you!</p>
+            <p className="gray-text">Thank you!</p>
 
-            <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+            <p className="gray-text">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
         </div>
     );
 }
